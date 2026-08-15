ac1e20e1de668417e3715b4ec39bd88ba8c45054
deniz paşık <denizpasik35@hotmail.com>
2026-08-15 12:23:48 +0300

    Add SQL solution

---
+++ 06_combining_subscription_tables/solution.sql
@@ -0,0 +1,38 @@
+/*
+Project: Combining Subscription Tables
+
+Objective:
+Combine active subscriptions from two product tables and calculate
+the number of subscriptions scheduled to expire in each year.
+
+SQL Concepts:
+- Common Table Expressions (CTEs)
+- UNION ALL
+- Filtering
+- DATE_TRUNC
+- Aggregate functions
+- GROUP BY
+*/
+
+WITH all_subscriptions AS (
+    SELECT
+        customerid,
+        expirationdate
+    FROM subscriptionsproduct1
+    WHERE active = 1
+
+    UNION ALL
+
+    SELECT
+        customerid,
+        expirationdate
+    FROM subscriptionsproduct2
+    WHERE active = 1
+)
+
+SELECT
+    DATE_TRUNC('year', expirationdate) AS expiration_year,
+    COUNT(*) AS subscriptions
+FROM all_subscriptions
+GROUP BY DATE_TRUNC('year', expirationdate)
+ORDER BY expiration_year;

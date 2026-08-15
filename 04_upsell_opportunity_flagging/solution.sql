7573a8348156cee258ea3225e65dc1d9c476d721
deniz paşık <denizpasik35@hotmail.com>
2026-08-15 12:13:29 +0300

    Add SQL solution

---
+++ 04_upsell_opportunity_flagging/solution.sql
@@ -0,0 +1,26 @@
+/*
+Project: Upsell Opportunity Flagging
+
+Objective:
+Identify customers who represent potential upsell opportunities
+based on their number of registered users or product subscriptions.
+
+SQL Concepts:
+- CASE statements
+- Aggregate functions
+- GROUP BY
+- Conditional logic
+*/
+
+SELECT
+    customerid,
+    COUNT(productid) AS num_products,
+    SUM(numberofusers) AS total_users,
+    CASE
+        WHEN SUM(numberofusers) >= 5000
+             OR COUNT(productid) = 1
+            THEN 1
+        ELSE 0
+    END AS upsell_opportunity
+FROM subscriptions
+GROUP BY customerid;
